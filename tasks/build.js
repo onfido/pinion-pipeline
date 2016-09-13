6a595509980124555e0ea69105df3e8d70d83a77
Stephen Cook <stephen@stephencookdev.co.uk>
2016-09-14 00:00:08 +0100

    Filter out empty task sequences, to prevent premature termination

--- tasks/build.js
+++ tasks/build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var gulpSequence = require('gulp-sequence');
 
 module.exports = function(config) {
@@ -28,15 +29,21 @@ module.exports = function(config) {
     return config.tasks[(task && task.configName) || task];
   };
 
+  var nonEmptyFilter = function(x) {
+    return x && !(Array.isArray(x) && x.length === 0);
+  };
+
   var buildDefaultTask = function(variant) {
     var applyVariant = applyUnboundVariant.bind(this, variant);
 
     return function(cb) {
-      gulpSequence(
+      var sequence = [
         assetTasks.filter(matchFilter).map(applyVariant),
         codeTasks.filter(matchFilter).map(applyVariant),
         cb
-      );
+      ].filter(nonEmptyFilter);
+
+      gulpSequence.apply(gulpSequence, sequence);
     };
   };
 
